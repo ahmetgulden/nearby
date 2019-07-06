97dd7d454208d09b563225273b7b54f213a3cf4a
ahmet gulden <ahm3tguld3n@gmail.com>
2019-07-06 10:11:40 +0300

    Rename view controller as map view controller

--- NearBy/Scenes/Map/MapViewController.swift
+++ NearBy/Scenes/Map/MapViewController.swift
@@ -1,20 +1,20 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  NearBy
 //
 //  Created by Ahmet Gülden on 6.07.2019.
 //  Copyright © 2019 Ahmet Gülden. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
+final class MapViewController: UIViewController {
+
+    @IBOutlet private weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
