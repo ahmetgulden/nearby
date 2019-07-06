50de6e128f58222596671b99b86efb5d7f9883be
ahmet gulden <ahm3tguld3n@gmail.com>
2019-07-06 09:57:28 +0300

    Create project

---
+++ NearBy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearBy
+//
+//  Created by Ahmet Gülden on 6.07.2019.
+//  Copyright © 2019 Ahmet Gülden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
