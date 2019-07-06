38702aa1cb016cb730231dc3d80a0dfe8e5a1231
ahmet gulden <ahm3tguld3n@gmail.com>
2019-07-06 19:58:28 +0300

    Add location struct

---
+++ NearBy/Models/Location.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  NearBy
+//
+//  Created by Ahmet Gülden on 6.07.2019.
+//  Copyright © 2019 Ahmet Gülden. All rights reserved.
+//
+
+import Foundation
+
+struct Location {
+
+    let latitude: Double
+    let longitude: Double
+}
