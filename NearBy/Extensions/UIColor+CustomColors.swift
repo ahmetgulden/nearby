6e8373978751469dced8a4f8a6d9296c7948f4fb
ahmet gulden <ahm3tguld3n@gmail.com>
2019-07-06 12:26:55 +0300

    Add custom colors extension

---
+++ NearBy/Extensions/UIColor+CustomColors.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor.swift
+//  NearBy
+//
+//  Created by Ahmet Gülden on 6.07.2019.
+//  Copyright © 2019 Ahmet Gülden. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static var nrb_backgroundColor: UIColor {
+        return UIColor(white: 0.9, alpha: 1.0)
+    }
+
+    static var nrb_oceanBlueColor: UIColor {
+        return UIColor(red: 65.0 / 255.0, green: 133.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0)
+    }
+}
