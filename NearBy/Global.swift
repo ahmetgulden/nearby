ad19881b5981df832a88bdaeb2e8dfe166797e51
ahmet gulden <ahm3tguld3n@gmail.com>
2019-07-06 12:25:08 +0300

    Add global constants file

---
+++ NearBy/Global.swift
@@ -0,0 +1,18 @@
+//
+//  Global.swift
+//  NearBy
+//
+//  Created by Ahmet Gülden on 6.07.2019.
+//  Copyright © 2019 Ahmet Gülden. All rights reserved.
+//
+
+import UIKit
+
+enum Global {
+
+    enum UI {
+
+        static let margin: CGFloat = 15.0
+        static let buttonHeight: CGFloat = 44.0
+    }
+}
